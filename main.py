5ce53ea00cc687bb9c4450f1bbe9ec6de9871f2e
Akshay2002Singh <06akshay2002@gmail.com>
2022-04-12 15:18:39 +0530

    init commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import socket 
+from IPy import IP
+
+def check_and_convert(ip_add):
+    try:
+        IP(ip_add)
+        return (ip_add)
+    except ValueError:
+        return socket.gethostbyname(ip_add)
+
+def check_port(t_ip,port):
+    try:
+        sock = socket.socket()
+        sock.settimeout(0.1)
+        sock.connect((t_ip,port))
+        print(f"\t[+] Port {port} is open")
+    except:
+        pass
+
+def scan(temp_ip):
+    real_ip = check_and_convert(temp_ip)
+    print(f"\n[-] Scanning Target {temp_ip}")
+    for port in range(1,100):
+        check_port(real_ip,port)
+
+if __name__=="__main__":
+    targets = input('\n[+] Enter Target/s To Scan (split multiple targets with ,) : ')
+    if ',' in targets:
+        for ip in targets.split(','):
+            scan(ip.strip(' '))
+    else:
+        scan(targets)
+
