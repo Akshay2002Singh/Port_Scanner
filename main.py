f742ebcc4368ef7693ebf193d95ffb3b2978f1ac
Akshay2002Singh <06akshay2002@gmail.com>
2022-04-12 15:43:00 +0530

    timeout updated

--- main.py
+++ main.py
@@ -12,16 +12,17 @@ def check_and_convert(ip_add):
 def check_port(t_ip,port):
     try:
         sock = socket.socket()
-        sock.settimeout(0.1)
+        sock.settimeout(0.9)
         sock.connect((t_ip,port))
         print(f"\t[+] Port {port} is open")
     except:
-        pass
+        print(f"\t[+] Port {port} is closed")
+        # pass
 
 def scan(temp_ip):
     real_ip = check_and_convert(temp_ip)
     print(f"\n[-] Scanning Target {temp_ip}")
-    for port in range(1,100):
+    for port in range(1,65536):
         check_port(real_ip,port)
 
 if __name__=="__main__":
